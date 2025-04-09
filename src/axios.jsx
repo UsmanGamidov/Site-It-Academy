7c014096f545a8eb413e4a90709861640c58139c
Gamidov Usman <118015879+UsmanGamidov@users.noreply.github.com>
2025-04-09 04:06:10 +0300

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: "http://localhost:3001"
+    baseURL: "https://site-it-academy-backend.onrender.com"
 })
 
 export default instance
