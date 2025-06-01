9fc984e0680f5a0563996fc3555fb5eaafdc55b2
UsmanGamidov <osmangamidov026@gmail.com>
2025-06-01 19:31:58 +0300

    build: обновил dist и добавил static.json

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 https://site-it-academy.onrender.com/
+
+
+yarn build
+git add .
+git commit -m "build: обновил dist и добавил static.json"
+git push origin main
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 👈 обязательно для корректной маршрутизации на Render
   plugins: [react()],
 })
